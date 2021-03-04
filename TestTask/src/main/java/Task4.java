e143d5ad22316922f79be5f28ca0bfe48df46069
BinoTegari <binofaata@gmail.com>
2021-03-04 21:10:45 +0400

    finish task4
    finally)

--- TestTask/src/main/java/Task4.java
+++ TestTask/src/main/java/Task4.java
@@ -1,21 +1,59 @@
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 
 public class Task4 {
-    public static void main(String[] args){
-        try{
-            InputStream inputStream = null;
-            HSSFWorkbook workbook = null;
-            inputStream = new FileInputStream("example.xls");
-            workbook = new HSSFWorkbook(inputStream);
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
+    public static void main(String[] args) {
+        System.out.println(ExcelParser.parse("TestTask/src/main/java/example.xls"));
+    }
+}
+
+class ExcelParser {
+
+    public static String parse(String fileName) {
+        String result = "";
+        InputStream inputStream;
+        HSSFWorkbook workBook = null;
+        try {
+            inputStream = new FileInputStream(fileName);
+            workBook = new HSSFWorkbook(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Sheet sheet = workBook.getSheetAt(0);
+        Iterator<Row> it = sheet.iterator();
+
+        while (it.hasNext()) {
+            Row row = it.next();
+            Iterator<Cell> cells = row.iterator();
+            while (cells.hasNext()) {
+                Cell cell = cells.next();
+                CellType cellType = cell.getCellType();
+                switch (cellType) {
+                    case STRING:
+                        result += cell.getStringCellValue() + "=";
+                        break;
+                    case NUMERIC:
+                        result += "[" + cell.getNumericCellValue() + "]";
+                        break;
+                    case FORMULA:
+                        result += "[" + cell.getNumericCellValue() + "]";
+                        break;
+                    default:
+                        result += "|";
+                        break;
+                }
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
+
